1111bef4c4a253437e51bb96db102d7ac1f5d03b
achiles665@gmail.com <achiles665@gmail.com>
2020-07-24 21:45:51 +0530

    front end with react-need to add database

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './containers/home';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import ContactUs from "./containers/ContactUs";
+import {BrowserRouter as Router,Switch,Route}from 'react-router-dom';
+import Post from './containers/Post';
+import Compose from './containers/Compose';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+      <Header />
+      <Hero />
+      <Route path="/" exact component={Home}/>
+      <Route path="/contact-us" component={ContactUs}/>
+      <Route path="/post/:postid" component={Post}/>
+      <Route path="/Compose31081437" component={Compose} />
     </div>
+    </Router>
+    
   );
 }
 
